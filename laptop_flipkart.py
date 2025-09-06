1266f1f143b57dbb602decca6cdd1b0b2a707dd0
MeghOffical <202301402@dau.ac.in>
2025-09-06 14:03:08 +0530

    adding code of selenium

---
+++ laptop_flipkart.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys 
+import time
+
+
+s = Service("C:/Users/MEGH BAVARVA/Desktop/chromedriver.exe")
+driver=webdriver.Chrome(service=s)
+
+#  Open a webpage
+driver.get("https://www.smartprix.com/laptops")
+time.sleep(2)
+
+
+driver.find_element(by=By.XPATH,value='//*[@id="app"]/main/aside/div/div[5]/div[2]/label[1]/input').click()
+time.sleep(1)
+
+driver.find_element(by=By.XPATH,value='//*[@id="app"]/main/aside/div/div[5]/div[2]/label[2]/input').click()
+time.sleep(2)
+
+
+old_height=driver.execute_script('return document.body.scrollHeight')
+
+while True:
+    
+    driver.find_element(by=By.XPATH,value='//*[@id="app"]/main/div[1]/div[2]/div[3]').click()
+    time.sleep(1)
+    
+    
+    new_height=driver.execute_script('return document.body.scrollHeight')
+    
+    print(old_height)
+    print(new_height)
+    
+    if new_height==old_height:
+        break
+    
+    old_height=new_height
+
+
+html=driver.page_source
+
+with open('smartprix.html','w',encoding='utf-8') as f:
+    f.write(html)
+
+input("Press Enter to close the browser...")
+driver.quit()
